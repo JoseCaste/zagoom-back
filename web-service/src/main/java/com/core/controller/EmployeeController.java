7836ff6d0b1eda325a165a7ed9754d1ede7a0437
JoseDev <jotacastellanosguz@gmail.com>
2024-05-24 11:56:06 -0600

    Se agrega get

--- web-service/src/main/java/com/core/controller/EmployeeController.java
+++ web-service/src/main/java/com/core/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import com.core.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,11 @@ public class EmployeeController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @GetMapping("/")
+    public ResponseEntity<?> get() throws Exception {
+        return ResponseEntity.ok().body("HI WORLD!!");
+    }
 
     @PostMapping("/")
     public ResponseEntity<?> doLogin(@RequestBody LoginDTO loginDTO) throws Exception {
